eb55db2c2edd3e886598c6621d13403676ef4ac5
chrstan23 <mamplatakyledeejay09@gmail.com>
2024-05-25 12:52:26 +0800

    Update the Laravel for Product and User Management and also SetUp Back Office React

--- betamart_laravel/betamart/app/Http/Controllers/ProductController.php
+++ betamart_laravel/betamart/app/Http/Controllers/ProductController.php
@@ -18,4 +18,33 @@ class ProductController extends Controller
         $product = Product::findOrFail($id);
         return response()->json($product);
     }
+
+        public function store(Request $request)
+    {
+         Product::create([
+                'name' => $request->name,
+                'price' => $request->price,
+                'description' => $request->description,
+        ]);
+
+        return response()->json(['message' => 'Product added successfully'], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->description = $request->input('description');
+        $product->save();
+
+        return response()->json(['message' => 'Product item updated successfully', 'product' => $product]);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return response()->json(['message' => 'Product removed successfully']);
+    }
 }
--- betamart_laravel/betamart/routes/api.php
+++ betamart_laravel/betamart/routes/api.php
@@ -4,15 +4,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\UserController;
 
 // api end point for user authorization
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// For user api end points
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/addUser', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/removeUser/{id}', [UserController::class, 'destroy']);
+
 // For product api end points
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/addProduct', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/removeProduct/{id}', [ProductController::class, 'destroy']);
 
 // For cart api end points
 Route::get('/carts', [CartController::class, 'index']);
